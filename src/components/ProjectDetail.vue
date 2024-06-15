bf26ec3b91fa4837e43fdcdb8e6ebf9415f84495
github Ishizuka2250 <squid2250@gmail.com>
2024-06-16 00:53:18 +0900

    Add:ProjectDetail.vue, DetailView.vue

---
+++ src/components/ProjectDetail.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>
+      Project Name
+    </h1>
+  </div>
+  <div>
+    <p>Project Info</p>
+    <img src="" alt="">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/DetailView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ProjectDetail from '../components/ProjectDetail.vue'
+</script>
+
+<template>
+  <main>
+    <ProjectDetail />
+  </main>
+</template>
